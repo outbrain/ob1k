d9267bcd4aa8aaf6c745f76f4eb9841e2b2d360d
eranharel <harel.eran@gmail.com>
2014-10-14 12:00:44 +0300

    Extracted spring context support from OB trunk, and bump spring version

---
+++ Ob1k/src/main/java/com/outbrain/ob1k/server/spring/SpringBeanContext.java
@@ -0,0 +1,27 @@
+package com.outbrain.ob1k.server.spring;
+
+import com.outbrain.ob1k.server.BeanContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import java.util.Map;
+
+/**
+ * User: aronen
+ * Date: 6/25/13
+ * Time: 6:50 PM
+ */
+public class SpringBeanContext implements BeanContext {
+  private final Map<String, AbstractApplicationContext> contexts;
+
+  public SpringBeanContext(final Map<String, AbstractApplicationContext> contexts) {
+    this.contexts = contexts;
+  }
+
+  public <T> T getBean(final String ctxName, final Class<T> type) {
+    return contexts.get(ctxName).getBean(type);
+  }
+
+  public <T> T getBean(final String ctxName, final String id, final Class<T> type) {
+    return contexts.get(ctxName).getBean(id, type);
+  }
+}
---
+++ Ob1k/src/main/java/com/outbrain/ob1k/server/spring/SpringContextBuilder.java
@@ -0,0 +1,107 @@
+package com.outbrain.ob1k.server.spring;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+/**
+ * User: aronen
+ * Date: 6/25/13
+ * Time: 6:28 PM
+ */
+public class SpringContextBuilder {
+  public static final String DEFAULT_CONTEXT_PATH = "";
+
+  private final List<CtxParams> subParams;
+  private final boolean allowBeanOverrideByDefault;
+  private CtxParams mainParams;
+  private String activeProfiles;
+
+  public SpringContextBuilder() {
+    this(DEFAULT_CONTEXT_PATH);
+  }
+
+  public SpringContextBuilder(final String contextPath) {
+    this(contextPath, true);
+  }
+
+  public SpringContextBuilder(final String contextPath, final boolean allowBeanOverrideByDefault) {
+    System.setProperty("com.outbrain.web.context.path", contextPath);
+    this.subParams = new ArrayList<>();
+    this.allowBeanOverrideByDefault = allowBeanOverrideByDefault;
+  }
+
+  private static class CtxParams {
+    final String key;
+    final String path;
+    private final boolean allowBeanOverride;
+
+    private CtxParams(final String key, final String path, final boolean allowBeanOverride) {
+      this.key = key;
+      this.path = path;
+      this.allowBeanOverride = allowBeanOverride;
+    }
+  }
+
+
+  public SpringContextBuilder setMainContext(final String name, final String path) {
+
+    return setMainContext(name,path, allowBeanOverrideByDefault);
+  }
+
+  public SpringContextBuilder setMainContext(final String name, final String path, final boolean allowBeanOverride) {
+    mainParams = new CtxParams(name, path, allowBeanOverride);
+    return this;
+  }
+
+  public SpringContextBuilder addSubContext(final String name, final String path) {
+    return addSubContext(name,path, allowBeanOverrideByDefault);
+  }
+
+  public SpringContextBuilder addSubContext(final String name, final String path, final boolean allowBeanOverride) {
+    subParams.add(new CtxParams(name, path, allowBeanOverride));
+    return this;
+  }
+  
+  public SpringContextBuilder setActiveProfiles(final String activeProfiles) {
+    this.activeProfiles = activeProfiles;
+    return this;
+  }
+
+  private AbstractApplicationContext createMainContext(final boolean allowBeanOverride) {
+    final GenericXmlApplicationContext context = new GenericXmlApplicationContext();
+    if (activeProfiles != null && !activeProfiles.isEmpty()) {
+      context.getEnvironment().setActiveProfiles(activeProfiles);
+    }
+    context.setAllowBeanDefinitionOverriding(allowBeanOverride);
+    context.load(mainParams.path);
+    context.refresh();
+    return context;
+  }
+
+  public AbstractApplicationContext createSubContext(final AbstractApplicationContext parent, final String path, final boolean allowBeanOverride) {
+    final GenericXmlApplicationContext subContext = new GenericXmlApplicationContext();
+    subContext.setParent(parent);
+    subContext.setAllowBeanDefinitionOverriding(allowBeanOverride);
+    subContext.load(path);
+    subContext.refresh();
+    return subContext;
+  }
+
+  public SpringBeanContext build() {
+    final Map<String, AbstractApplicationContext> contexts = new HashMap<>();
+
+    final AbstractApplicationContext mainCtx = createMainContext(mainParams.allowBeanOverride);
+    contexts.put(mainParams.key, mainCtx);
+    for(final CtxParams param: subParams) {
+      final AbstractApplicationContext subCtx = createSubContext(mainCtx, param.path, param.allowBeanOverride);
+      contexts.put(param.key, subCtx);
+    }
+
+    return new SpringBeanContext(contexts);
+  }
+}
