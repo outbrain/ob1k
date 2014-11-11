d25119d6b5b2190c0655686d54f0de2d45e1265e
haski <asy.ronen@gmail.com>
2014-11-11 20:27:31 +0200

    adding missing files.

---
+++ ob1k-concurrent/src/main/java/com/outbrain/ob1k/concurrent/combiners/FutureTriFunction.java
@@ -0,0 +1,12 @@
+package com.outbrain.ob1k.concurrent.combiners;
+
+import com.outbrain.ob1k.concurrent.ComposableFuture;
+
+/**
+ * Created by aronen on 11/11/14.
+ * <p/>
+ * same as a TriFunction but allows for a long running computation to be returned.
+ */
+public interface FutureTriFunction<T1, T2, T3, R> {
+  ComposableFuture<R> apply(T1 first, T2 second, T3 third);
+}
