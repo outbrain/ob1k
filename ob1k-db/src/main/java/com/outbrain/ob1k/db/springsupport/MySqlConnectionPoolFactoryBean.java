cf9f74374f749ba75204fb26fbcdc166cd08ddd1
eranharel <harel.eran@gmail.com>
2015-03-29 13:53:23 +0300

    Added MySql connection pool optional spring support

---
+++ ob1k-db/src/main/java/com/outbrain/ob1k/db/springsupport/MySqlConnectionPoolFactoryBean.java
@@ -0,0 +1,67 @@
+package com.outbrain.ob1k.db.springsupport;
+
+import com.outbrain.ob1k.db.DbConnectionPool;
+import com.outbrain.ob1k.db.MySqlConnectionPoolBuilder;
+import com.outbrain.swinfra.metrics.api.MetricFactory;
+import org.springframework.beans.factory.FactoryBean;
+
+/**
+ * Spring support for MySqlConnectionPool creation
+ * @author Eran Harel
+ */
+public class MySqlConnectionPoolFactoryBean implements FactoryBean {
+
+  private final MySqlConnectionPoolBuilder builder;
+
+  public MySqlConnectionPoolFactoryBean(final String connectionString, final String username) {
+    builder = MySqlConnectionPoolBuilder.newBuilder(connectionString, username);
+  }
+
+  @Override
+  public Object getObject() throws Exception {
+    return builder.build();
+  }
+
+  @Override
+  public Class<?> getObjectType() {
+    return DbConnectionPool.class;
+  }
+
+  @Override
+  public boolean isSingleton() {
+    return true;
+  }
+
+  public void setDatabase(final String database) {
+    builder.forDatabase(database);
+  }
+
+  public void setPassword(final String password) {
+    builder.password(password);
+  }
+
+  public void setConnectTimeoutSeconds(final long connectTimeoutSeconds) {
+    builder.connectTimeoutSeconds(connectTimeoutSeconds);
+  }
+
+  public void setMaxConnections(final int maxConnections) {
+    builder.maxConnections(maxConnections);
+  }
+
+  public void setMaxQueueSize(final Integer maxQueueSize) {
+    builder.maxQueueSize(maxQueueSize);
+  }
+
+  public void setMaxIdleTimeMs(final Integer maxIdleTimeMs) {
+    builder.maxIdleTimeMs(maxIdleTimeMs);
+  }
+
+  public void setValidationIntervalMs(final Integer validationIntervalMs) {
+    builder.validationIntervalMs(validationIntervalMs);
+  }
+
+  public void setMetricFactory(final MetricFactory metricFactory) {
+    builder.withMetrics(metricFactory);
+  }
+
+}
