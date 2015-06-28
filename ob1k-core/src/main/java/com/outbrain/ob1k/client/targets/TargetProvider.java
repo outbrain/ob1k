6e02c579caa4e5ef88529ffaa6c0720e1e36d9fb
Guy Marom <marom.guy@gmail.com>
2015-06-28 12:19:28 +0300

    Fixing typo

--- ob1k-core/src/main/java/com/outbrain/ob1k/client/targets/TargetProvider.java
+++ ob1k-core/src/main/java/com/outbrain/ob1k/client/targets/TargetProvider.java
@@ -1,7 +1,7 @@
 package com.outbrain.ob1k.client.targets;
 
 /**
- * An API for retrieving tagets for the client invocations.
+ * An API for retrieving targets for the client invocations.
  * An example target may be a static list of hosts + round robin, or a discovery based provider.
  * @author eran 6/21/15.
  */
