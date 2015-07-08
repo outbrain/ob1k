ffa307b5fa9cf3ff557a5abeb1e9446e0a90495b
eranharel <harel.eran@gmail.com>
2015-07-08 11:54:37 +0300

    Initial ob1k-consul check-in

---
+++ ob1k-consul/src/main/java/com/outbrain/ob1k/consul/ConsulAPI.java
@@ -0,0 +1,55 @@
+package com.outbrain.ob1k.consul;
+
+import com.outbrain.ob1k.HttpRequestMethodType;
+import com.outbrain.ob1k.client.ClientBuilder;
+import com.outbrain.ob1k.client.targets.SimpleTargetProvider;
+import com.outbrain.ob1k.common.marshalling.ContentType;
+
+/**
+ * @author Eran Harel
+ */
+public class ConsulAPI {
+
+  public static ConsulServiceRegistry getServiceRegistry() {
+    return ConsulServiceRegistryHolder.INSTANCE;
+  }
+
+  public static ConsulCatalog getCatalog() {
+    return ConsulCatalogHolder.INSTANCE;
+  }
+
+  private static class ConsulServiceRegistryHolder {
+    private static final ConsulServiceRegistry INSTANCE = createServiceRegistry();
+
+    private static ConsulServiceRegistry createServiceRegistry() {
+      return new ClientBuilder<>(ConsulServiceRegistry.class)
+              .setTargetProvider(new SimpleTargetProvider("http://localhost:8500/v1/agent/service/"))
+              .bindEndpoint("deregister", HttpRequestMethodType.GET, "deregister/{serviceId}")
+              .setProtocol(ContentType.JSON)
+              .setRequestTimeout(1000)
+              .build();
+    }
+  }
+
+  private static class ConsulCatalogHolder {
+    private static final ConsulCatalog INSTANCE = createCatalog();
+
+    private static ConsulCatalog createCatalog() {
+      return new ClientBuilder<>(ConsulCatalog.class)
+              .setTargetProvider(new SimpleTargetProvider("http://localhost:8500/v1/catalog"))
+              .bindEndpoint("findInstances", HttpRequestMethodType.GET, "/service/{service}?dc={dc}")
+              .bindEndpoint("findDcLocalInstances", HttpRequestMethodType.GET, "/service/{service}")
+              .bindEndpoint("pollDcLocalInstances", HttpRequestMethodType.GET, "/service/{service}?tag={filterTag}&index={index}&wait={maxWaitSec}s")
+              .bindEndpoint("services", HttpRequestMethodType.GET, "/services?dc={dc}")
+              .setProtocol(ContentType.JSON)
+              .setRequestTimeout(10000)
+              .build();
+    }
+  }
+
+  public static void main(final String[] args) throws Exception {
+
+    System.out.println(getCatalog().findInstances("MyApp", "dc1").get());
+    System.out.println(getCatalog().findDcLocalInstances("MyApp").get());
+  }
+}
---
+++ ob1k-consul/src/main/java/com/outbrain/ob1k/consul/ConsulCatalog.java
@@ -0,0 +1,20 @@
+package com.outbrain.ob1k.consul;
+
+import com.outbrain.ob1k.Service;
+import com.outbrain.ob1k.concurrent.ComposableFuture;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * A programmatic API that maps to the /v1/catalog/* consul REST API
+ * @author Eran Harel
+ */
+public interface ConsulCatalog extends Service {
+  ComposableFuture<Map<String, Set<String>>> services(final String dc);
+  ComposableFuture<List<ServiceInstance>> findInstances(final String service, final String dc);
+  ComposableFuture<List<ServiceInstance>> findDcLocalInstances(final String service);
+  ComposableFuture<List<ServiceInstance>> pollDcLocalInstances(final String service, final String filterTag, final long index, final int maxWaitSec);
+  ComposableFuture<Set<String>> datacenters();
+}
