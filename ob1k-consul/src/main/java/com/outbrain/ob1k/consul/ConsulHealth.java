a276bd549b2fb7a8b25399bf7804753d5180c294
Michael Arenzon <misha9090@gmail.com>
2015-08-03 17:48:21 +0300

    more endpoints for health in ConsulApi

--- ob1k-consul/src/main/java/com/outbrain/ob1k/consul/ConsulHealth.java
+++ ob1k-consul/src/main/java/com/outbrain/ob1k/consul/ConsulHealth.java
@@ -7,10 +7,31 @@ import java.util.List;
 
 /**
  * A programmatic API that maps to the /v1/health/* consul REST API
+ *
  * @author Eran Harel
  */
 public interface ConsulHealth extends Service {
 
-    ComposableFuture<List<HealthInfoInstance>> filterDcLocalHealthyInstances(final String service, final String filterTag);
-    ComposableFuture<List<HealthInfoInstance>> pollHealthyInstances(final String service, final String filterTag, final long index);
+  ComposableFuture<List<HealthInfoInstance>> filterDcLocalHealthyInstances(final String service, final String filterTag);
+
+  ComposableFuture<List<HealthInfoInstance>> pollHealthyInstances(final String service, final String filterTag, final long index);
+
+  ComposableFuture<List<HealthInfoInstance>> pollInstancesHealth(final String service, final String dc);
+
+  ComposableFuture<List<HealthInfoInstance.Check>> pollInstancesChecks(final String service, final String dc);
+
+  ComposableFuture<List<HealthInfoInstance.Check>> pollInstancesAtState(final States state, final String dc);
+
+  enum States {
+    ANY,
+    UNKNOWN,
+    PASSING,
+    WARNING,
+    CRITICAL;
+
+    @Override
+    public String toString() {
+      return name().toLowerCase();
+    }
+  }
 }
