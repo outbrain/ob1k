fabb85da691d59d93a4e47ae6f50115a296ea0ed
eranharel <harel.eran@gmail.com>
2015-08-04 14:03:50 +0300

    Allow using consul index from a single response when polling for health

--- ob1k-consul/src/main/java/com/outbrain/ob1k/consul/ConsulHealth.java
+++ ob1k-consul/src/main/java/com/outbrain/ob1k/consul/ConsulHealth.java
@@ -2,6 +2,7 @@ package com.outbrain.ob1k.consul;
 
 import com.outbrain.ob1k.Service;
 import com.outbrain.ob1k.concurrent.ComposableFuture;
+import com.outbrain.ob1k.http.TypedResponse;
 
 import java.util.List;
 
@@ -14,7 +15,7 @@ public interface ConsulHealth extends Service {
 
   ComposableFuture<List<HealthInfoInstance>> filterDcLocalHealthyInstances(final String service, final String filterTag);
 
-  ComposableFuture<List<HealthInfoInstance>> pollHealthyInstances(final String service, final String filterTag, final long index);
+  ComposableFuture<TypedResponse<List<HealthInfoInstance>>> pollHealthyInstances(final String service, final String filterTag, final long index);
 
   ComposableFuture<List<HealthInfoInstance>> getInstancesHealth(final String service, final String dc);
 
