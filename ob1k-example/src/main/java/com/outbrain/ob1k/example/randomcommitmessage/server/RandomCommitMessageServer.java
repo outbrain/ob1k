75e935e3ecf067f7ffc5cef738aa68c92e98af30
eranharel <harel.eran@gmail.com>
2015-08-20 16:37:54 +0300

    Random commit message server example WIP

---
+++ ob1k-example/src/main/java/com/outbrain/ob1k/example/randomcommitmessage/server/RandomCommitMessageServer.java
@@ -0,0 +1,56 @@
+package com.outbrain.ob1k.example.randomcommitmessage.server;
+
+import com.outbrain.ob1k.server.Server;
+import com.outbrain.ob1k.server.build.ServerBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.outbrain.ob1k.HttpRequestMethodType.*;
+
+/**
+ * Main class for the random commit message server.
+ * Sets up a service that will fetch random commit messages from http://whatthecommit.com/
+ *
+ * @author Eran Harel
+ */
+public class RandomCommitMessageServer {
+
+  private static final Logger logger = LoggerFactory.getLogger(RandomCommitMessageServer.class);
+
+  public static final int PORT = 8080;
+  public static final String CTX_PATH = "/rcm";
+  public static final String SERVICE_PATH = "/whatthecommit";
+
+  Server server;
+
+  public static void main(final String[] args) {
+    new RandomCommitMessageServer().start(PORT);
+  }
+
+  public void start(final int port) {
+    server = buildServer(port);
+    server.start();
+    logger.info("## {} is started on port: {} ##", getClass().getSimpleName(), port);
+  }
+
+  public void stop() {
+    if (server != null) {
+      server.stop();
+    }
+  }
+
+  private Server buildServer(final int port) {
+    return ServerBuilder.newBuilder().
+      configurePorts(builder -> builder.setPort(port)).
+      setContextPath(CTX_PATH).
+      withServices(builder -> builder.defineService(new RandomCommitMessageService(), SERVICE_PATH, serviceBuilder -> {
+        serviceBuilder.addEndpoint(GET, "single", "/single");
+        serviceBuilder.addEndpoint(GET, "multi", "/multi/{numMessages}");
+      })).
+      configureExtraParams(builder -> builder.setRequestTimeout(2000, TimeUnit.MILLISECONDS)).
+      build();
+  }
+
+}
---
+++ ob1k-example/src/main/java/com/outbrain/ob1k/example/randomcommitmessage/server/RandomCommitMessageService.java
@@ -0,0 +1,64 @@
+package com.outbrain.ob1k.example.randomcommitmessage.server;
+
+import com.google.common.base.Function;
+import com.outbrain.ob1k.Service;
+import com.outbrain.ob1k.concurrent.ComposableFuture;
+import com.outbrain.ob1k.concurrent.ComposableFutures;
+import com.outbrain.ob1k.concurrent.Try;
+import com.outbrain.ob1k.concurrent.handlers.FutureResultHandler;
+import com.outbrain.ob1k.http.HttpClient;
+import com.outbrain.ob1k.http.RequestBuilder;
+import com.outbrain.ob1k.http.Response;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A service that will fetch random commit messages from http://whatthecommit.com/
+ * @author Eran Harel
+ */
+public class RandomCommitMessageService implements Service {
+
+  private final HttpClient httpClient = HttpClient.createDefault();
+
+  private final RequestBuilder requestBuilder = httpClient.get("http://whatthecommit.com/index.txt");
+
+  public ComposableFuture<List<String>> multi(final int numMessages) {
+    final List<ComposableFuture<String>> futureMessages = new ArrayList<>(numMessages);
+
+    for (int i = 0; i < numMessages; i++) {
+      final ComposableFuture<String> messageFuture = requestBuilder.asResponse().transform(new Function<Response, String>() {
+        @Override
+        public String apply(final Response response) {
+          try {
+            return response.getResponseBody();
+          } catch (final IOException e) {
+            return "Failed to fetch message...";
+          }
+        }
+      });
+
+      futureMessages.add(messageFuture);
+    }
+
+    return ComposableFutures.all(futureMessages).materialize();
+  }
+
+  public ComposableFuture<String> single() {
+    return requestBuilder.asResponse().continueWith(new FutureResultHandler<Response, String>() {
+      @Override
+      public ComposableFuture<String> handle(final Try<Response> responseTry) {
+        if(responseTry.isSuccess()) {
+          try {
+            return ComposableFutures.fromValue(responseTry.getValue().getResponseBody());
+          } catch (IOException e) {
+            return ComposableFutures.fromError(e);
+          }
+        } else {
+          return ComposableFutures.fromError(responseTry.getError());
+        }
+      }
+    });
+  }
+}
