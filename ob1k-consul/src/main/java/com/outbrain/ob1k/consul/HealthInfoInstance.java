5c68c14a9d35dae9c1c5ed0acbd47aa8b1e0c8f4
eranharel <harel.eran@gmail.com>
2015-11-30 12:37:00 +0200

    hashCode / equals

--- ob1k-consul/src/main/java/com/outbrain/ob1k/consul/HealthInfoInstance.java
+++ ob1k-consul/src/main/java/com/outbrain/ob1k/consul/HealthInfoInstance.java
@@ -4,68 +4,149 @@ import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 /**
  * A single value in the Consul <code>/v1/health/service/{service}</code> response
+ *
  * @author Eran Harel
  */
 public class HealthInfoInstance {
 
-    public Node Node;
-    public Service Service;
-    public List<Check> Checks;
+  public Node Node;
+  public Service Service;
+  public List<Check> Checks;
+
+  @Override
+  public String toString() {
+    return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).toString();
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (!(o instanceof HealthInfoInstance)) {
+      return false;
+    }
+
+    final HealthInfoInstance other = (HealthInfoInstance)o;
+
+    return Objects.equals(Node, other.Node) && Objects.equals(Service, other.Service) && Objects.equals(Checks, other.Checks);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(Node, Service, Checks);
+  }
+
+  public static class Service {
+    public String ID;
+    public String Service;
+    public Set<String> Tags;
+    public String Address;
+    public long Port;
+
+    public Integer port(final String portType) {
+      return TagsUtil.extractPort(Tags, portType);
+    }
+
+    public String context() {
+      return TagsUtil.extractContextPath(Tags);
+    }
 
     @Override
     public String toString() {
-        return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).toString();
+      return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).toString();
     }
 
+    @Override
+    public boolean equals(final Object o) {
+      if (this == o) {
+        return true;
+      }
 
-    public static class Service {
-        public String ID;
-        public String Service;
-        public Set<String> Tags;
-        public String Address;
-        public long Port;
+      if (!(o instanceof Service)) {
+        return false;
+      }
 
-        public Integer port(final String portType) {
-            return TagsUtil.extractPort(Tags, portType);
-        }
+      final Service other = (Service)o;
 
-        public String context() {
-            return TagsUtil.extractContextPath(Tags);
-        }
-
-        @Override
-        public String toString() {
-            return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).toString();
-        }
+      return Objects.equals(ID, other.ID) && Objects.equals(Address, other.Address) && Objects.equals(Port, other.Port);
     }
 
-    public static class Node {
-        public String Node;
-        public String Address;
+    @Override
+    public int hashCode() {
+      return Objects.hash(ID, Address, Port);
+    }
+  }
 
-        @Override
-        public String toString() {
-            return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).toString();
-        }
+  public static class Node {
+    public String Node;
+    public String Address;
+
+    @Override
+    public String toString() {
+      return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).toString();
     }
 
-    public static class Check {
-        public String Node;
-        public String CheckID;
-        public String Name;
-        public String Status;
-        public String Notes;
-        public String Output;
-        public String ServiceID;
-        public String ServiceName;
+    @Override
+    public boolean equals(final Object o) {
+      if (this == o) {
+        return true;
+      }
 
-        @Override
-        public String toString() {
-            return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).toString();
-        }
+      if (!(o instanceof Node)) {
+        return false;
+      }
+
+      final Node other = (Node)o;
+
+      return Objects.equals(Node, other.Node);
     }
+
+    @Override
+    public int hashCode() {
+      return Objects.hashCode(Node);
+    }
+  }
+
+  public static class Check {
+    public String Node;
+    public String CheckID;
+    public String Name;
+    public String Status;
+    public String Notes;
+    public String Output;
+    public String ServiceID;
+    public String ServiceName;
+
+    @Override
+    public String toString() {
+      return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).toString();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+      if (this == o) {
+        return true;
+      }
+
+      if (!(o instanceof Check)) {
+        return false;
+      }
+
+      final Check other = (Check)o;
+
+      return Objects.equals(CheckID, other.CheckID) && Objects.equals(Status, other.Status);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(CheckID, Status);
+    }
+  }
 }
