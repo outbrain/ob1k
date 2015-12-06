995b3f909027b1fd8b85267964cebcb5a948ef28
Guy Marom <marom.guy@gmail.com>
2015-12-06 09:23:23 +0200

    Renaming DoNothingMetricFactory to DummyMetricFactory

--- util-metrics/src/test/java/com/outbrain/swinfra/metrics/DummyMetricFactory.java
+++ util-metrics/src/test/java/com/outbrain/swinfra/metrics/DummyMetricFactory.java
@@ -4,9 +4,9 @@ import com.outbrain.swinfra.metrics.api.MetricFactory;
 import org.mockito.Answers;
 import org.mockito.Mockito;
 
-public class DoNothingMetricFactory {
+public class DummyMetricFactory {
 
-  public static MetricFactory doNothingMetricFactory() {
+  public static MetricFactory newDummyMetricFactory() {
     return Mockito.mock(MetricFactory.class, Answers.RETURNS_MOCKS.get());
   }
 
