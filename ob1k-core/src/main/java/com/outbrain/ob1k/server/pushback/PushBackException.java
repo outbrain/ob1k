cbf5b6a6edcc345e6d2f1992bbc976cba600e80c
Amir Langer <langera@gmail.com>
2016-01-11 10:41:27 +0200

    remove pushback filter

--- ob1k-core/src/main/java/com/outbrain/ob1k/server/pushback/PushBackException.java
+++
@@ -1,8 +0,0 @@
-package com.outbrain.ob1k.server.pushback;
-
-public class PushBackException extends RuntimeException {
-
-  public PushBackException(final String message) {
-    super(message);
-  }
-}
