a1d7617b3d3ab6ed37f368e33726a206f96e44e8
Michael Arenzon <misha9090@gmail.com>
2016-04-14 01:48:11 +0300

    fixing marshalling single param

--- ob1k-core/src/main/java/com/outbrain/ob1k/common/marshalling/MessagePackRequestMarshaller.java
+++ ob1k-core/src/main/java/com/outbrain/ob1k/common/marshalling/MessagePackRequestMarshaller.java
@@ -23,13 +23,9 @@ public class MessagePackRequestMarshaller extends JacksonRequestMarshaller {
 
   @Override
   public byte[] marshallRequestParams(final Object[] requestParams) throws IOException {
-    // requests can come from a regular httpClient post request with a single param that get wrapped inside an array
-    // or in case of a real RPC call with a single param. in both cases we unwrap it and send it as is.
-    // the code in unmarshallRequestParams() know how to deal with both single object or array of objects.
-    final Object params = requestParams == null ? new Object[0] :
-      requestParams.length == 1 ? requestParams[0] : requestParams;
-
-    return marshallingStrategy.marshall(params);
+    // Not sending simple object in case of MessagePack because previous implementation
+    // have sent always array of objects, even in case of single object. So it's backward-compatible.
+    return marshallingStrategy.marshall(requestParams);
   }
 
   private static ObjectMapper createObjectMapper() {
