1fded8b2e782d2d2fa4f460731b30fe25e2a5bf3
eranharel <harel.eran@gmail.com>
2016-04-18 12:51:55 +0300

    Added support for generic collections in  the messagepack transcoder

--- ob1k-cache/src/main/java/com/outbrain/ob1k/cache/memcache/folsom/MessagePackTranscoder.java
+++ ob1k-cache/src/main/java/com/outbrain/ob1k/cache/memcache/folsom/MessagePackTranscoder.java
@@ -3,6 +3,7 @@ package com.outbrain.ob1k.cache.memcache.folsom;
 import com.spotify.folsom.Transcoder;
 import org.apache.commons.lang.SerializationException;
 import org.msgpack.MessagePack;
+import org.msgpack.template.Template;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -14,16 +15,28 @@ public class MessagePackTranscoder<T> implements Transcoder<T> {
 
   private final MessagePack messagePack;
   private final Class<T> valueType;
+  private final Template<T> template;
 
   public MessagePackTranscoder(final MessagePack messagePack, final Class<T> valueType) {
     this.messagePack = Objects.requireNonNull(messagePack, "messagePack must not be null");
     this.valueType = Objects.requireNonNull(valueType, "valueType must not be null");
+    template = null;
+  }
+
+  public MessagePackTranscoder(final MessagePack messagePack, final Template<T> template) {
+    this.messagePack = Objects.requireNonNull(messagePack, "messagePack must not be null");
+    this.template = Objects.requireNonNull(template, "template must not be null");
+    valueType = null;
   }
 
   @Override
   public T decode(final byte[] b) {
     try {
-      return messagePack.read(b, valueType);
+      if (template == null) {
+        return messagePack.read(b, valueType);
+      } else {
+        return messagePack.read(b, template);
+      }
     } catch (final IOException e) {
       throw new SerializationException("Failed to decode to type " + valueType.getSimpleName(), e);
     }
--- ob1k-cache/src/test/java/com/outbrain/ob1k/cache/memcache/folsom/MessagePackTranscoderTest.java
+++ ob1k-cache/src/test/java/com/outbrain/ob1k/cache/memcache/folsom/MessagePackTranscoderTest.java
@@ -4,13 +4,19 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.msgpack.MessagePack;
+import org.msgpack.template.Template;
+import org.msgpack.template.Templates;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Eran Harel
  */
 public class MessagePackTranscoderTest {
   private final MessagePack messagePack = new MessagePack();
-  private final MessagePackTranscoder<Person> transcoder = new MessagePackTranscoder<>(messagePack, Person.class);
 
   @Before
   public void setup() {
@@ -19,12 +25,27 @@ public class MessagePackTranscoderTest {
 
   @Test
   public void testRoundTripTranscoding() {
+    final MessagePackTranscoder<Person> transcoder = new MessagePackTranscoder<>(messagePack, Person.class);
+
     final Person person = new Person(55, "Saba", 99);
     final Person decodedPerson = transcoder.decode(transcoder.encode(person));
     Assert.assertEquals("decoded person age", person.age, decodedPerson.age);
     Assert.assertEquals("decoded person name", person.name, decodedPerson.name);
     Assert.assertEquals("decoded person weight", person.weight, decodedPerson.weight, 0.01f);
     Assert.assertNotSame(person, decodedPerson);
+  }
+
+  @Test
+  public void testRoundTripTranscoding_map() throws IOException {
+    final Template<Map<String, Integer>> mapTemplate = Templates.tMap(Templates.TString, Templates.TInteger);
+    final MessagePackTranscoder<Map<String, Integer>> t = new MessagePackTranscoder<Map<String, Integer>>(messagePack, mapTemplate);
+
+    final Map<String, Integer> map = new HashMap<>();
+    map.put("1", 1);
+    map.put("2", 2);
+
+    final Map<String, Integer> decodedMap = t.decode(t.encode(map));
+    Assert.assertEquals(map, decodedMap);
   }
 
 }
