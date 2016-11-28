5dd7d9838dbeaeb54174c130dd4946565fa4c100
eranharel <harel.eran@gmail.com>
2016-11-28 12:07:19 +0200

    Null check and docs (without the bogus additions)

--- ob1k-cache/src/main/java/com/outbrain/ob1k/cache/CacheLoader.java
+++ ob1k-cache/src/main/java/com/outbrain/ob1k/cache/CacheLoader.java
@@ -3,6 +3,7 @@ package com.outbrain.ob1k.cache;
 import com.outbrain.ob1k.concurrent.ComposableFuture;
 
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * User: aronen
@@ -14,7 +15,16 @@ public interface CacheLoader<K,V> {
   ComposableFuture<Map<K, V>> load(String cacheName, Iterable<? extends K> keys);
 
 
+  /**
+   * Creates a {@link CacheLoader} from a {@link TypedCache}
+   * @param cache the wrapped cache
+   * @param <K>
+   * @param <V>
+   * @return a CacheLoader wrapping the cache passed as argument
+   */
   static <K, V> CacheLoader<K, V> fromTypedCache(final TypedCache<K, V> cache) {
+    Objects.requireNonNull(cache, "cache must not be null");
+
     return new CacheLoader<K, V>() {
       @Override
       public ComposableFuture<V> load(final String cacheName, final K key) {
