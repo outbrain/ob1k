f5a6988051ec4b222d781dc36b19c88c082b869b
Amir Hadadi <ahadadi@outbrain.com>
2017-03-18 22:37:27 +0200

    Previous penalty mechanism didn't adjust for a fast drop in maximal number of pending requests. Adding adjustment for this.

--- ob1k-consul/src/main/java/com/outbrain/ob1k/consul/CooperativeTargetProvider.java
+++ ob1k-consul/src/main/java/com/outbrain/ob1k/consul/CooperativeTargetProvider.java
@@ -1,5 +1,6 @@
 package com.outbrain.ob1k.consul;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -29,6 +30,7 @@ public class CooperativeTargetProvider extends ConsulBasedTargetProvider {
   private final Multiset<String> pendingRequests = ConcurrentHashMultiset.create();
   private final Multiset<String> remainingPenalty = ConcurrentHashMultiset.create();
   private final AtomicInteger roundRobin = new AtomicInteger();
+  private volatile int maxPendingRequests;
 
   public CooperativeTargetProvider(final HealthyTargetsList healthyTargetsList, final String urlSuffix, final Map<String, Integer> tag2weight, final int penalty) {
     super(healthyTargetsList, urlSuffix, tag2weight);
@@ -37,42 +39,63 @@ public class CooperativeTargetProvider extends ConsulBasedTargetProvider {
 
   @Override
   public List<String> provideTargetsImpl(final int targetsNum, final List<String> currTargets) {
-    // We assume that only the first target is likely to be used, the other targets
-    // may serve for double dispatch. That's why we increment the round robin counter
-    // by only one.
-    final int index = roundRobin.getAndIncrement();
+    final Set<String> providedTargetsSet = createTargetsPool(targetsNum, currTargets);
 
-    // get this target closer to being removed from the blacklist.
-    remainingPenalty.remove(currTargets.get(Math.abs(index % currTargets.size())));
-
-    // Maintain the order of the elements so that we can tie break them
-    // by the round robin order.
-    final Set<String> providedTargetsSet = new LinkedHashSet<>();
-
-    final int targetsPool = Math.min(targetsNum + EXTRA_TARGETS, currTargets.size());
-    for (int i = 0; i < targetsPool; i++) {
-      providedTargetsSet.add(currTargets.get(Math.abs((index + i) % currTargets.size())));
-    }
-
-    final List<String> providedTargets = bestTargets(targetsNum, providedTargetsSet);
+    final List<String> providedTargets = new ArrayList<>(bestTargets(targetsNum, providedTargetsSet));
 
     for (int i=0; providedTargets.size() < targetsNum; i++) {
-      providedTargets.add(providedTargets.get(i % providedTargets.size()));
+      providedTargets.add(providedTargets.get(i));
     }
 
     return providedTargets;
   }
 
+  private Set<String> createTargetsPool(int targetsNum, List<String> currTargets) {
+    // We assume that only the first target is likely to be used, the other targets
+    // may serve for double dispatch. That's why we increment the round robin counter
+    // by one instead of targetsNum.
+    final int index = roundRobin.getAndIncrement();
+
+    // Maintain the order of the elements so that we can tie break them
+    // by the round robin order.
+    final Set<String> providedTargetsSet = new LinkedHashSet<>();
+
+    // limit the target pool size to keep the complexity O(targetsNum).
+    final int targetsPool = Math.min(targetsNum + EXTRA_TARGETS, currTargets.size());
+    int maxPendingRequests = 0;
+    for (int i = 0; i < targetsPool; i++) {
+      final String target = currTargets.get(Math.abs((index + i) % currTargets.size()));
+      if (i == 0) {
+        decreasePenalty(target);
+      }
+      providedTargetsSet.add(target);
+      maxPendingRequests = Integer.max(maxPendingRequests, pendingRequests.count(target));
+    }
+    this.maxPendingRequests = maxPendingRequests;
+    return providedTargetsSet;
+  }
+
+  private void decreasePenalty(String target) {
+    final int remainingPenalty = this.remainingPenalty.count(target);
+    if (remainingPenalty > 0) {
+      // Using the least of remainingPenalty - 1, penalize(target) is done to handle the case
+      // where a target was penalized heavily because the max number of
+      // pending requests was high at the time, but then the maximum decreased
+      // markedly, and its penalty is now too high.
+      this.remainingPenalty.setCount(target, Integer.min(remainingPenalty - 1, penalize(target)));
+    }
+  }
+
   private List<String> bestTargets(int targetsNum, Set<String> providedTargetsSet) {
     // create snapshots so that the comparator will not switch the order of targets during sorting.
-    Map<String, Integer> pendingRequests = Maps.newHashMapWithExpectedSize(targetsNum);
+    Map<String, Integer> pendingRequests = Maps.newHashMapWithExpectedSize(providedTargetsSet.size());
     providedTargetsSet.forEach(target -> pendingRequests.put(target, this.pendingRequests.count(target)));
 
-    Map<String, Integer> roundRobinPosition = Maps.newHashMapWithExpectedSize(targetsNum);
-    providedTargetsSet.forEach(target -> roundRobinPosition.put(target, roundRobinPosition.size()));
-
-    Map<String, Integer> remainingPenalty = Maps.newHashMapWithExpectedSize(targetsNum);
+    Map<String, Integer> remainingPenalty = Maps.newHashMapWithExpectedSize(providedTargetsSet.size());
     providedTargetsSet.forEach(target -> remainingPenalty.put(target, this.remainingPenalty.count(target)));
+
+    Map<String, Integer> roundRobinPosition = Maps.newHashMapWithExpectedSize(providedTargetsSet.size());
+    providedTargetsSet.forEach(target -> roundRobinPosition.put(target, roundRobinPosition.size()));
 
     return Ordering.from(Comparator.<String, Integer>comparing(target -> remainingPenalty.get(target) + pendingRequests.get(target)).
                     thenComparing(roundRobinPosition::get)).
@@ -94,18 +117,10 @@ public class CooperativeTargetProvider extends ConsulBasedTargetProvider {
 
   public void targetDispatchEnded(String target, boolean success) {
     pendingRequests.remove(target);
-    if (success) {
-      remainingPenalty.elementSet().remove(target);
-    } else {
-      remainingPenalty.setCount(target, penalize(target));
-    }
+    remainingPenalty.setCount(target, success ? 0 : penalize(target));
   }
 
   private int penalize(String target) {
-    final int maxPendingRequests = pendingRequests.entrySet().stream().
-            mapToInt(Multiset.Entry::getCount).
-            max().orElse(0);
-
     return penalty + Math.max(maxPendingRequests - pendingRequests.count(target), 0);
   }
 }
--- ob1k-consul/src/test/java/com/outbrain/ob1k/consul/CooperativeTargetProviderTest.java
+++ ob1k-consul/src/test/java/com/outbrain/ob1k/consul/CooperativeTargetProviderTest.java
@@ -1,10 +1,12 @@
 package com.outbrain.ob1k.consul;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,15 +14,13 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import com.google.common.collect.ImmutableSet;
-
 /**
  * Created by ahadadi on 18/03/2017.
  */
 @RunWith(MockitoJUnitRunner.class)
 public class CooperativeTargetProviderTest {
 
-  private static final List<String> TARGETS = asList("t1", "t2", "t3");
+  private static final List<String> TARGETS = asList("t1", "t2");
   private static final int PENALTY = 2;
 
   @Mock
@@ -36,31 +36,49 @@ public class CooperativeTargetProviderTest {
   @Test
   public void testReturnByNumberOfPendingRequests() {
     provider.targetDispatched("t1");
-    assertEquals(ImmutableSet.of("t2", "t3"), ImmutableSet.copyOf(provider.provideTargetsImpl(2, TARGETS)));
+    assertEquals(singletonList("t2"), provider.provideTargetsImpl(1, TARGETS));
+  }
+
+  @Test
+  public void testReturnTheSameTargetMultipleTimes() {
+    assertEquals(asList("t1", "t2", "t1"), provider.provideTargetsImpl(3, TARGETS));
+  }
+
+  @Test
+  public void testClearPenaltiesOnTargetsChange() {
+    provider.targetDispatchEnded("t1", false);
+    provider.onTargetsChanged(Collections.emptyList());
+    assertEquals(singletonList("t1"), provider.provideTargetsImpl(1, TARGETS));
   }
 
   @Test
   public void testPenaltyDecay() {
     provider.targetDispatchEnded("t1", false);
-    assertEquals(ImmutableSet.of("t2"), ImmutableSet.copyOf(provider.provideTargetsImpl(1, TARGETS)));
-    provider.targetDispatched("t2");
-    assertEquals(ImmutableSet.of("t3"), ImmutableSet.copyOf(provider.provideTargetsImpl(1, TARGETS)));
-    provider.targetDispatched("t3");
-    assertEquals(ImmutableSet.of("t3"), ImmutableSet.copyOf(provider.provideTargetsImpl(1, TARGETS)));
-    provider.targetDispatched("t3");
-    assertEquals(ImmutableSet.of("t1"), ImmutableSet.copyOf(provider.provideTargetsImpl(1, TARGETS)));
+    IntStream.range(0, PENALTY).forEach(i -> assertEquals(singletonList("t2"), provider.provideTargetsImpl(1, TARGETS)));
+    assertEquals(singletonList("t1"), provider.provideTargetsImpl(1, TARGETS));
+  }
+
+  @Test
+  public void testDecreasePenaltyWhenTheMaximumPendingRequestsDecreases() {
+    final int requests = 10;
+    IntStream.range(0, requests).forEach(i -> provider.targetDispatched("t2"));
+
+    // t1 should now have a high penalty to make it non competitive with t2.
+    provider.targetDispatchEnded("t1", false);
+
+    // t1's penalty should decrease as t2 responded for all requests.
+    IntStream.range(0, requests).forEach(i -> provider.targetDispatchEnded("t2", true));
+
+    IntStream.range(0, PENALTY).forEach(i -> assertEquals(singletonList("t2"), provider.provideTargetsImpl(1, TARGETS)));
+    assertEquals(singletonList("t1"), provider.provideTargetsImpl(1, TARGETS));
   }
 
   @Test
   public void testRoundRobin() {
-    assertEquals(ImmutableSet.of("t1"), ImmutableSet.copyOf(provider.provideTargetsImpl(1, TARGETS)));
-    assertEquals(ImmutableSet.of("t2"), ImmutableSet.copyOf(provider.provideTargetsImpl(1, TARGETS)));
-    assertEquals(ImmutableSet.of("t3"), ImmutableSet.copyOf(provider.provideTargetsImpl(1, TARGETS)));
+    assertEquals(singletonList("t1"), provider.provideTargetsImpl(1, TARGETS));
+    assertEquals(singletonList("t2"), provider.provideTargetsImpl(1, TARGETS));
 
-    assertEquals(ImmutableSet.of("t1"), ImmutableSet.copyOf(provider.provideTargetsImpl(1, TARGETS)));
-    assertEquals(ImmutableSet.of("t2"), ImmutableSet.copyOf(provider.provideTargetsImpl(1, TARGETS)));
-    assertEquals(ImmutableSet.of("t3"), ImmutableSet.copyOf(provider.provideTargetsImpl(1, TARGETS)));
+    assertEquals(singletonList("t1"), provider.provideTargetsImpl(1, TARGETS));
+    assertEquals(singletonList("t2"), provider.provideTargetsImpl(1, TARGETS));
   }
-
-
 }
