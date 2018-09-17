923fc7a8d31701f0e7b843b0b1ab953cc3136027
Guy Kobrinsky <gkobrinsky@outbrain.com>
2018-09-17 14:58:44 +0300

    print post and put content

--- ob1k-crud/src/main/java/com/outbrain/ob1k/crud/audit/LogAudit.kt
+++ ob1k-crud/src/main/java/com/outbrain/ob1k/crud/audit/LogAudit.kt
@@ -1,21 +1,22 @@
 package com.outbrain.ob1k.crud.audit
 
 import com.outbrain.ob1k.HttpRequestMethodType
+import com.outbrain.ob1k.HttpRequestMethodType.*
 import com.outbrain.ob1k.Request
 import com.outbrain.ob1k.Response
 import org.slf4j.LoggerFactory
 
 class LogAudit : ICrudAudit {
     private val logger = LoggerFactory.getLogger(LogAudit::class.java)
-    var methods: Set<HttpRequestMethodType> = mutableSetOf(HttpRequestMethodType.POST, HttpRequestMethodType.PUT, HttpRequestMethodType.DELETE)
+    var methods: Set<HttpRequestMethodType> = mutableSetOf(POST, PUT, DELETE)
 
     override fun audit(username: String?, request: Request, response: Response) {
         if (methods.contains(request.method)) {
-            val created = when (request.method) {
-                HttpRequestMethodType.POST -> ": ${response.rawContent?.replace("\\s".toRegex(), "") ?: ""}"
+            val updated = when (request.method) {
+                POST, PUT -> ": ${response.rawContent?.replace("\\s".toRegex(), "") ?: ""}"
                 else -> ""
             }
-            logger.info("$username ${request.method} ${request.path} $created")
+            logger.info("$username ${request.method} ${request.path} $updated")
         }
     }
 }
--- ob1k-crud/src/main/java/com/outbrain/ob1k/crud/audit/SlackAudit.kt
+++ ob1k-crud/src/main/java/com/outbrain/ob1k/crud/audit/SlackAudit.kt
@@ -1,6 +1,7 @@
 package com.outbrain.ob1k.crud.audit
 
 import com.outbrain.ob1k.HttpRequestMethodType
+import com.outbrain.ob1k.HttpRequestMethodType.*
 import com.outbrain.ob1k.Request
 import com.outbrain.ob1k.Response
 import com.outbrain.ob1k.http.HttpClient
@@ -10,16 +11,16 @@ class SlackAudit(private val httpClient: HttpClient,
                  private val url: String,
                  private val prefix: String) : ICrudAudit {
 
-    var methods: Set<HttpRequestMethodType> = mutableSetOf(HttpRequestMethodType.POST, HttpRequestMethodType.PUT, HttpRequestMethodType.DELETE)
+    var methods: Set<HttpRequestMethodType> = mutableSetOf(POST, PUT, DELETE)
 
     override fun audit(username: String?, request: Request, response: Response) {
         username?.let {
             if (methods.contains(request.method)) {
-                val created = when (request.method) {
-                    HttpRequestMethodType.POST -> ": ${response.rawContent?.replace("\\s".toRegex(), "") ?: ""}"
+                val updated = when (request.method) {
+                    POST, PUT -> ":\n${response.rawContent?.replace("\\s".toRegex(), "") ?: ""}"
                     else -> ""
                 }
-                val message = "$prefix $username ${request.method} ${request.uri} $created"
+                val message = "$prefix $username ${request.method} ${request.uri} $updated"
                 httpClient.post(url).setContentType(ContentType.JSON).setBody(SlackMessage(message)).asResponse()
             }
         }
